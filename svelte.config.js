abce53a1c1f30eff8edc65b7c9f1416c4b5ada46
michecode <mfrs@protonmail.com>
2023-01-14 19:04:05 -0800

    move to sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,16 @@
-import preprocess from 'svelte-preprocess';
-import seqPreprocessor from 'svelte-sequential-preprocessor';
-import { preprocessThrelte } from '@threlte/preprocess';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: seqPreprocessor([preprocess(), preprocessThrelte()]),
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter()
+	},
+	preprocess: vitePreprocess(),
 };
 
 export default config;
