4238791b4658f18961242620175c393e1eb0aba2
michecode <mfrs@protonmail.com>
2023-01-14 18:21:53 -0800

    init

---
+++ svelte.config.js
@@ -0,0 +1,9 @@
+import preprocess from 'svelte-preprocess';
+import seqPreprocessor from 'svelte-sequential-preprocessor';
+import { preprocessThrelte } from '@threlte/preprocess';
+
+const config = {
+  preprocess: seqPreprocessor([preprocess(), preprocessThrelte()]),
+};
+
+export default config;
