018187c2f61ba0aa4e246892b9541d7c328bb27e
michecode <mfrs@protonmail.com>
2023-01-22 13:27:45 -0800

    prep new icons

---
+++ src/lib/TechChip.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Icon from './Icon.svelte';
+
+	export let text: string;
+	export let iconKey: string;
+</script>
+
+<div>
+	<p>{text}</p>
+	<Icon type={iconKey} />
+</div>
--- src/lib/projectDetails.ts
+++ src/lib/projectDetails.ts
@@ -8,8 +8,38 @@ export const projects = {
 		link: 'https://alto.earth',
 		image: AltoGif,
 		isVideo: false,
-		body: 'plants!!',
-		techStack: ['Svelte', 'Astro', 'Tailwind', 'Sanity'],
+		body: `Alto was my original inspiration to pursue web development. 
+		When I first started expanding my indoor array of plants I kept forgetting how to care for them and searching 
+		for multiple different plants caused a lot of friction. 
+		I decided to take it on as a project, simplify and reduce as much of that annoying friction as possible. 
+		I succeeded in that task, I loved the user flow and design. I was able to get back in touch my illustration skills 
+		and add those as I went along development. However, I got distracted and never filled out the database how I originally wanted 
+		to. Recently, I rekindled the inspiration and decided to rebuild it from the ground up with an entire new tech stack now 
+		that I have become much more experienced. I chose to use this as an opportunity to learn Svelte and Astro, also using 
+		my usual favorites, Tailwind and Typescript.
+		`,
+		techStack: [
+			{
+				name: 'Svelte',
+				logoKey: 'svelteLogo',
+			},
+			{
+				name: 'Astro',
+				logoKey: 'astroLogo',
+			},
+			{
+				name: 'tailwind',
+				logoKey: 'tailwindLogo',
+			},
+			{
+				name: 'TypeScript',
+				logoKey: 'typescriptLogo',
+			},
+			{
+				name: 'Sanity',
+				logoKey: 'package',
+			},
+		],
 	},
 	chese: {
 		title: 'chese',
@@ -18,15 +48,46 @@ export const projects = {
 		isVideo: true,
 		body: 'nfts!!',
 		techStack: [
-			'React',
-			'TypeScript',
-			'Tailwind',
-			'Gatsby',
-			'Solidity',
-			'Hardhat',
-			'Alchemy',
-			'Infura',
-			'Pinata',
+			{
+				name: 'React',
+				logoKey: 'reactLogo',
+			},
+			{
+				name: 'TypeScript',
+				logoKey: 'typescriptLogo',
+			},
+			{
+				name: 'Tailwind',
+				logoKey: 'tailwindLogo',
+			},
+			{
+				name: 'Gatsby',
+				logoKey: 'gatsbyLogo',
+			},
+			{
+				name: 'Solidity',
+				logoKey: 'solidityLogo',
+			},
+			{
+				name: 'Hardhat',
+				logoKey: 'package',
+			},
+			{
+				name: 'Alchemy',
+				logoKey: 'api',
+			},
+			{
+				name: 'Infura',
+				logoKey: 'api',
+			},
+			{
+				name: 'IPFS',
+				logoKey: 'api',
+			},
+			{
+				name: 'Pinata',
+				logoKey: 'api',
+			},
 		],
 	},
 	lahvah: {
@@ -35,6 +96,23 @@ export const projects = {
 		image: LahvahGif,
 		isVideo: false,
 		body: 'mdoels!!',
-		techStack: ['React', 'NextJS', 'Tailwind', 'TypeScript'],
+		techStack: [
+			{
+				name: 'React',
+				logoKey: 'reactLogo',
+			},
+			{
+				name: 'NextJS',
+				logoKey: 'nextjsLogo',
+			},
+			{
+				name: 'Tailwind',
+				logoKey: 'tailwindLogo',
+			},
+			{
+				name: 'TypeScript',
+				logoKey: 'typescriptLogo',
+			},
+		],
 	},
 };
