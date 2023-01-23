971bbae4ae897b031bca40a6c9ea1b208c68950c
michecode <mfrs@protonmail.com>
2023-01-22 22:33:56 -0800

    background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,16 @@
 <script>
 	import '../app.css';
-	import { onMount } from 'svelte';
 	import Suminigashi from '$lib/images/background.png';
 	import SuminigashiMobile from '$lib/images/background-mobile.png';
-
-	let isDesktop = true;
-	onMount(() => {
-		isDesktop = window.screen.availWidth >= 1024;
-	});
 </script>
 
 <!-- svelte-ignore a11y-missing-attribute -->
 <div class="bg-black absolute left-0 top-0 -z-50 h-screen w-screen overflow-hidden">
+	<!-- 2 components so i dont have an image flash on first render! too lazy to fix the roation -->
+	<img src={Suminigashi} class="hidden lg:block opacity-80 h-full w-full animate-breathe" />
 	<img
-		src={isDesktop ? Suminigashi : SuminigashiMobile}
-		class="opacity-80 h-full w-full animate-breathe"
+		src={SuminigashiMobile}
+		class="block lg:hidden opacity-80 h-full w-full animate-mobileBreathe"
 	/>
 </div>
 <slot />
